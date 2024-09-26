c6fd848743915c99866282d8b7a2a8df82ca0b91
jason gomez <143572848+jgomez1015@users.noreply.github.com>
2024-09-26 12:52:44 -0700

    finished the body, wrote the case and break functions for each command, putting together the sample code and my idea direction, now i test and debug

--- chavviCalc.java
+++ chavviCalc.java
@@ -9,13 +9,31 @@ public class chavviCalculator {
 
     Scanner scan = new Scanner(System.in);
     Character command = '_';
+    double A = 0;
+    double B = 0; 
 
-    while (true) {
+    while (command ! = 'q') {
+        printMenu(A, B);
+        System.out.print("Enter a command: ");
+        command = menuGetCommand(scan);
+
+        if (!executeCommand(scan, command, A, B)) {
+            System.out.println("Invalid command. Please try again from available commands. ");
+        }
+    }
+    scan.close();
+    }
+        private static void printMenuLine() {
         System.out.println("--------------------------");
+        }
+        private static void printMenuCommand(Character command, String desc) {
+            System.out.printf("%s\t%s\n", command, desc);
+        }
+        public static void printMenu(double A, double B) {
         System.out.println(" Chavvi Calculatinator ");
-        System.out.println("--------------------------");
+        printMenuLine();
         System.out.println(" A = " + A , " B = " + B);
-        System.out.println("--------------------------");
+        printMenuLine(); 
         System.out.println("a      Enter a Value for A");
         System.out.println("b      Enter a Value for B");
         System.out.println("+      Add");
@@ -24,10 +42,63 @@ public class chavviCalculator {
         System.out.println("/      Divide");
         System.out.println("c      Clear");
         System.out.println("q      Quit");
-        System.out.println("--------------------------");
-        System.out.println("\n");
-        System.out.println("Enter a command: ");     
+        printMenuLine();
+        System.out.println("\n");         
     }        
 
+        private static Character menuGetCommand(Scanner scan) { 
+            Character command = '_';
+            String rawInput = scan.nextLine(); 
+
+            if (rawInput.length() > 0) {
+                rawInput = rawInput.toLowerCase();
+                command = rawInput.charAt(0);
+            }
+            return command; 
+        }
+        
+        private static Boolean executeCommand(Scanner scan, Character command, double A, double B) {
+            Boolean success = true; 
+
+            switch (command) {
+                case 'a': 
+                System.out.print("Enter value for A: ");
+                A = scan.nextDouble(); 
+                scan.nextline();
+                break; 
+                case 'b': 
+                System.out.print("Enter value for B: ");
+                B = scan.nextDouble();
+                scan.nextLine(); 
+                break; 
+                case '+';
+                System.out.println("Result is: " + (A + B));
+                break; 
+                case '-';
+                System.out.println("Result is: " + (A - B));
+                break; 
+                case '*';
+                System.out.println("Result is: " + (A * B));
+                break; 
+                case '/';
+                if (B != 0) {
+                    System.out.print("Result is: " + (A / B ));
+                } else {
+                    System.out.println("Error: Can't divide by zero silly!");
+                }
+                break; 
+                case 'c': 
+                A = 0;
+                B = 0; 
+                System.out.println("Values reset for A and B.");
+                break; 
+                case 'q': 
+                System.out.println("Thanks for using ChavviCalc!");
+                System.out.println("Run this again whenever needed and take care!");
+                break; 
+            default: 
+                success = false; 
+            }
+            return success; 
+        }
     }
-}
