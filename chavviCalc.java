439dcb3a9c1d7b20d45637cc032a3276fcc7d221
jason gomez <143572848+jgomez1015@users.noreply.github.com>
2024-09-26 12:23:31 -0700

    still working on the functionality

---
+++ chavviCalc.java
@@ -0,0 +1,33 @@
+// jason gomez, student id; 1046059 
+// start of chavviCalc program
+
+package.com.chavviCalc1015; 
+import java.util.Scanner; 
+
+public class chavviCalculator { 
+    public static void main(String [args]) {
+
+    Scanner scan = new Scanner(System.in);
+    Character command = '_';
+
+    while (true) {
+        System.out.println("--------------------------");
+        System.out.println(" Chavvi Calculatinator ");
+        System.out.println("--------------------------");
+        System.out.println(" A = " + A , " B = " + B);
+        System.out.println("--------------------------");
+        System.out.println("a      Enter a Value for A");
+        System.out.println("b      Enter a Value for B");
+        System.out.println("+      Add");
+        System.out.println("-      Subtract");
+        System.out.println("*      Multiply");
+        System.out.println("/      Divide");
+        System.out.println("c      Clear");
+        System.out.println("q      Quit");
+        System.out.println("--------------------------");
+        System.out.println("\n");
+        System.out.println("Enter a command: ");     
+    }        
+
+    }
+}
