0659ba4696b4052606a563d7e0c8ff9db2fedc04
jason gomez <143572848+jgomez1015@users.noreply.github.com>
2024-09-26 19:22:35 -0700

    fixed syntax errors; []args* instead of [args] and colons instead of semi colons on the case statements, also i forgot to capitalize a L in nextLine

--- chavviCalc.java
+++ chavviCalc.java
@@ -5,7 +5,7 @@ package.com.chavviCalc1015;
 import java.util.Scanner; 
 
 public class chavviCalculator { 
-    public static void main(String [args]) {
+    public static void main(String [] args) {
 
     Scanner scan = new Scanner(System.in);
     Character command = '_';
@@ -32,7 +32,7 @@ public class chavviCalculator {
         public static void printMenu(double A, double B) {
         System.out.println(" Chavvi Calculatinator ");
         printMenuLine();
-        System.out.println(" A = " + A , " B = " + B);
+        System.out.println(" A = " + A + " B = " + B);
         printMenuLine(); 
         System.out.println("a      Enter a Value for A");
         System.out.println("b      Enter a Value for B");
